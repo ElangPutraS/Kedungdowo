021b16c7242e3da448207f6da89d4444e0b57c84
El <elangputrasartika@rocketmail.com>
2019-08-04 05:19:06 +0700

    new composer

---
+++ database/migrations/2018_07_25_063456_alter_mail_add_more_metadata_columns.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMailAddMoreMetadataColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mail', function (Blueprint $table) {
+            $table->text('sender')->nullable();
+            $table->text('cc')->nullable();
+            $table->text('bcc')->nullable();
+            $table->text('reply_to')->nullable();
+            $table->unsignedSmallInteger('priority')->nullable();
+            $table->string('content_type', 255)->default('text/plain');
+        });
+    }
+}
