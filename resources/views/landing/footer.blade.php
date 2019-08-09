57412ce46da2c1fefe780830fc76f92e09ad68b4
Hanif Naufal <hanifnfl27@gmail.com>
2019-08-09 18:48:58 +0700

    Tampak depan

---
+++ resources/views/landing/footer.blade.php
@@ -0,0 +1,53 @@
+<footer class="footer-distributed">
+
+    <div class="footer-left">
+
+        <h3>Company<span>logo</span></h3>
+
+        <p class="footer-links">
+            <a href="#" class="link-1">Home</a>
+
+            <a href="#">About</a>
+
+            <a href="#">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Company Name © 2015</p>
+    </div>
+
+    <div class="footer-center">
+
+        <div>
+            <i class="map marker alternate icon"></i>
+            <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
+        </div>
+
+        <div>
+            <i class="phone icon"></i>
+            <p>+1.555.555.5555</p>
+        </div>
+
+        <div>
+            <i class="envelope icon"></i>
+            <p><a href="mailto:support@company.com">support@company.com</a></p>
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+        <h4>Kepala Desa</h4>
+        <img style="width: 100px; height: 100px; border-radius: 50%" src="{{asset('img/kdap.jpg')}}">
+        <h5>Tumbar Sudarso</h5>
+
+        <div class="footer-icons">
+
+            <a href="#"><i class="facebook icon"></i></a>
+            <a href="#"><i class="twitter icon"></i></a>
+            <a href="#"><i class="linkedin icon"></i></a>
+            <a href="#"><i class="github icon"></i></a>
+
+        </div>
+
+    </div>
+
+</footer>
